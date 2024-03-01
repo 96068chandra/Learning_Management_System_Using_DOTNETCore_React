c21614f51b6a029ccabd2bb8fcc1aa167b2ef297
chandrashekarj <chandrashekarj@ICS-LT-1XM1DN3>
2024-03-01 19:33:08 +0530

    Add project files.

---
+++ Learning_Management_System_Using_DOTNETCore_React/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learning_Management_System_Using_DOTNETCore_React.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
